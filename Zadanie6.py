3ad3984de2f10dc217acd672e9fc6203a102f836
Krzysiek0709 <trolek700@gmail.com>
2021-03-11 08:03:59 +0100

    Zadanie 6

--- Zadanie6.py
+++ Zadanie6.py
@@ -1,2 +1,3 @@
+
 piosenka = "Just la la la la la It goes around the world Just la la la la la It's all around the world"
 print(piosenka.count("la"))
---
+++ Zadanie8.py
@@ -0,0 +1,3 @@
+wyraz = "Just la la la la la It goes around the world Just la la la la la It's all around the world"
+x = wyraz.split()
+print(x)
